52444f627069240f769004be87bb03632f91162e
Sineth Shashintha <sinethshashintha2000@gmail.com>
2023-09-08 22:24:59 +0530

    jsx

--- exercise2/src/App.js
+++ exercise2/src/App.js
@@ -4,10 +4,16 @@ import NavBar from './components/NavBar';
 import SimpleCard from './components/SimpleCard';
 
 function App() {
+
+  const url = "https://picsum.photos/id/237/300";
+  const alt = "dog";
+
   return (
     <>
-    <SimpleCard />
-    <NavBar />
+    {/*<img src={url} alt={alt} /> */}
+    <h1 style = {{
+      color: "white",
+    }}>Hello world</h1>
     </>
   );
 }
