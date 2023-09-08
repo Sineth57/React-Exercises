4d6fad3c2f23325dadfb2cf18c1b6f1d468440b2
Sineth Shashintha <sinethshashintha2000@gmail.com>
2023-09-08 17:15:51 +0530

    created first react UI

--- exercise2/src/components/SimpleCard.js
+++ exercise2/src/components/SimpleCard.js
@@ -1,10 +1,13 @@
+import "./SimpleCard.css";
+
 function SimpleCard(){
     return(
         <div className="card-container">
 
             <h1>Introduction</h1>
-            <p>This is my first react UI.</p>
+            <p>This is my first react UI Using SimpleCard Component.</p>
             <button>View More</button>
+
         </div>
     );
 }
