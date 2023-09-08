41580169d36641897238e0bec5f1029e080b929a
Sineth Shashintha <sinethshashintha2000@gmail.com>
2023-09-08 17:00:48 +0530

    First react UI

---
+++ exercise2/src/components/SimpleCard.js
@@ -0,0 +1,12 @@
+function SimpleCard(){
+    return(
+        <div className="card-container">
+
+            <h1>Introduction</h1>
+            <p>This is my first react UI.</p>
+            <button>View More</button>
+        </div>
+    );
+}
+
+export default SimpleCard;
